0bba90439d3a5ca68c515cff485ac2a8ecf5bad2
Shubh-0213 <148485039+Shubh-0213@users.noreply.github.com>
2024-01-31 20:17:23 +0530

    Add files via upload

--- Project-1/database/login_page.sql
+++ Project-1/database/login_page.sql
@@ -1,1 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 31, 2024 at 02:53 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `small_projects`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_page`
+--
+
+CREATE TABLE `login_page` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `login_page`
+--
+
+INSERT INTO `login_page` (`id`, `name`, `date`, `email`, `password`) VALUES
+(1, 'Shubhdeep', '2005-09-02', 'shubhu0213@gmail.com', 'esdrftgyhu'),
+(2, 'Shubhdeep', '2000-05-04', 'shubhu0213@gmail.com', 'zdftyghuij');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login_page`
+--
+ALTER TABLE `login_page`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `login_page`
+--
+ALTER TABLE `login_page`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
