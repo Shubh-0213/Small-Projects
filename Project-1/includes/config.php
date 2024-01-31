0e7f31975b587aa492834fca2ab0ea6c1ef31561
Shubh-0213 <148485039+Shubh-0213@users.noreply.github.com>
2024-01-31 20:14:58 +0530

    Add files via upload

--- Project-1/includes/config.php
+++ Project-1/includes/config.php
@@ -1,1 +1,15 @@
+<?php
 
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'small_projects';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if($conn -> connect_error ){
+    $connect_error = $conn -> connect_error;
+    echo $connect_error;
+}
+
+?>
